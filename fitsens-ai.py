d638205637bed7fbd87006cf0a8d347bb0b864c6
Thanumalaiyan25 <thanumalaiyanviswa@gmail.com>
2025-10-30 04:26:42 +0530

    Update fitsens-ai.py

--- fitsens-ai.py
+++ fitsens-ai.py
@@ -687,7 +687,7 @@ with tabs[3]:
         uploaded_file = st.file_uploader("Upload a photo", type=["jpg", "png", "jpeg"])
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
-            st.image(image, caption="Uploaded Image", use_column_width=True)
+            st.image(image, caption="Uploaded Image", use_container_width=True)
             if st.button("Analyze Image"):
                 body_type, ratio = analyze_image(image)
                 if body_type != "Unknown":
